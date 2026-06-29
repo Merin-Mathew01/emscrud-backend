3beb6e0a0d4a91b7a46c6e335e3201998a2b955e
Merin Mathew <Merin.Mathew@nestgroup.net>
2026-06-29 11:22:40 +0530

    ADD - comments

--- controllers/employeeController.js
+++ controllers/employeeController.js
@@ -1,6 +1,7 @@
+// Read employee data from db.json
 const data = require('../db.json')
 
-
+// Add employee controller - data from the form is taken from the req body and pushed into the array in db.json
 const addEmployee = (req,res)=>{
     try{
         const newEmployee={...req.body}
@@ -11,11 +12,13 @@ const addEmployee = (req,res)=>{
     }
 }
 
+// get a single employee details - employee details get using find method
 const getEmployeeDetails = (req,res)=>{
     const employeeDetails = data.employees.find(emp=>emp.id==req.params.id)
     res.status(200).json(employeeDetails)
 }
 
+// delete employee - delete done using filter 
 const deleteEmployee = (req,res)=>{
     try {
         data.employees=data.employees.filter(emp=>emp.id !=req.params.id)
@@ -25,6 +28,7 @@ const deleteEmployee = (req,res)=>{
     }
 }
 
+// update employee details - employeedetails get using find and each field is updated 
 const updateEmployee = (req,res)=>{
     try {
         const employee = data.employees.find(emp=>emp.id==req.params.id)
@@ -41,31 +45,27 @@ const updateEmployee = (req,res)=>{
     }
 }
 
+// getallemployees - single controller for search,pagination
 const getAllemployees = (req,res)=>{
     const page = Number(req.query.page)
     const limit = Number(req.query.limit)
+    // calculate start index and end index for pagination
     const startIndex = (page-1) * limit
     const endIndex = startIndex + limit
     let allEmployees = data.employees
     try {
         const searchText = req.query.search
+        // filter employees based on search
         if(searchText){
-            allEmployees = allEmployees.filter(emp=>emp.name.toLowerCase().includes(searchText.toLowerCase()) || emp.department.toLowerCase().includes(searchText.toLowerCase()))
+            allEmployees = allEmployees.filter(emp=>emp.name.toLowerCase().includes(searchText.toLowerCase()))
         }
         const totalRecords = allEmployees.length
+        // get only the employess for the curent page
         const paginatedEmployees = allEmployees.slice(startIndex,endIndex)
-        res.status(200).json({employees:paginatedEmployees,total:totalRecords})
-        
-        
-        
+        res.status(200).json({employees:paginatedEmployees,total:totalRecords})        
     } catch (error) {
         console.log(error);
     }
 }
-
-
-
-
-
 
 module.exports = {getAllemployees,addEmployee,deleteEmployee,getEmployeeDetails,updateEmployee}
--- router.js
+++ router.js
@@ -3,14 +3,19 @@ const {getAllemployees, addEmployee, deleteEmployee, getEmployeeDetails, updateE
 
 const route = express.Router()
 
+// get employee(includessearch and pagination)
 route.get('/employees',getAllemployees)
 
+// add employee
 route.post('/employees',addEmployee)
 
+// delete employee
 route.delete('/employee/:id',deleteEmployee)
 
+// get single employee details
 route.get('/employee/:id',getEmployeeDetails)
 
+// update employee details
 route.put('/employee/:id',updateEmployee)
 
 module.exports = route
